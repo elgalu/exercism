4ef1300433433f4cf4d7a8701922a0071ca31dc5
Leo Gallucci <elgalu3@gmail.com>
2013-08-09 01:53:22 -0300

    Iteration 3: http://goo.gl/YOT9ir

--- ruby/bob/bob.rb
+++ ruby/bob/bob.rb
@@ -1,5 +1,35 @@
 class Bob
   def hey(message)
+    return 'Fine. Be that way!' if says_nothing?(message)
+    return 'Woah, chill out!'   if yelling?(message)
+    return 'Sure.'              if questioning?(message)
+    return 'Whatever.'
+  end
+
+  def says_nothing?(message)
+    message.to_s.strip == ''
+  end
+
+  def yelling?(message)
+    message == message.upcase
+  end
+
+  def questioning?(message)
+    message.end_with?('?')
+  end
+
+  # Provided sample alternative to returns:
+  def hey_using_case(message)
+    case
+    when says_nothing?(message) then 'Fine. Be that way!'
+    when yelling?(message)      then 'Woah, chill out!'
+    when questioning?(message)  then 'Sure.'
+    else                             'Whatever.'
+    end
+  end
+
+  # Provided sample alternative to returns:
+  def hey_using_ifs(message)
     if says_nothing?(message)
       'Fine. Be that way!'
     elsif yelling?(message)
@@ -9,24 +39,5 @@ class Bob
     else
       'Whatever.'
     end
-  end
-
-  def hey_using_returns(message)
-    return 'Fine. Be that way!' if says_nothing?(message)
-    return 'Woah, chill out!'   if yelling?(message)
-    return 'Sure.'              if questioning?(message)
-    return 'Whatever.'
-  end
-
-  def says_nothing?(message)
-    message.nil? || message.strip == ''
-  end
-
-  def yelling?(message)
-    message == message.upcase
-  end
-
-  def questioning?(message)
-    message[-1] == '?'
   end
 end
