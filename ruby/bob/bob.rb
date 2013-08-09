7097aa75804b0411b47d85b2be63ccaf9a32003d
Leo Gallucci <elgalu3@gmail.com>
2013-08-09 01:52:10 -0300

    Iteration 2: http://goo.gl/kkn2WC

--- ruby/bob/bob.rb
+++ ruby/bob/bob.rb
@@ -1,13 +1,32 @@
 class Bob
   def hey(message)
-    if message.nil? || message.strip == ''
+    if says_nothing?(message)
       'Fine. Be that way!'
-    elsif message == message.upcase
+    elsif yelling?(message)
       'Woah, chill out!'
-    elsif message[-1] == '?'
+    elsif questioning?(message)
       'Sure.'
     else
       'Whatever.'
     end
+  end
+
+  def hey_using_returns(message)
+    return 'Fine. Be that way!' if says_nothing?(message)
+    return 'Woah, chill out!'   if yelling?(message)
+    return 'Sure.'              if questioning?(message)
+    return 'Whatever.'
+  end
+
+  def says_nothing?(message)
+    message.nil? || message.strip == ''
+  end
+
+  def yelling?(message)
+    message == message.upcase
+  end
+
+  def questioning?(message)
+    message[-1] == '?'
   end
 end
